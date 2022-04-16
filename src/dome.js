bbb0be1e4815b25802802c1728f5d8b7927b6efc
lfnull <1754443646@qq.com>
2022-04-16 23:22:55 +0800

    1.0

---
+++ src/dome.js
@@ -0,0 +1,13 @@
+window.dom = {
+    find(node) {
+        return document.querySelectorAll(node)
+    },
+    style(node, name, value) {
+        node.style[name] = value
+    },
+    each(nodeList, fn) {
+        for (let i = 0; i < nodeList.length; i++) {
+            fn.call(null, nodeList)
+        }
+    }
+};
